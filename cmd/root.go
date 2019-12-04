212f0e457c0bfb88e17c204f5402dffc844f857f
181192 <k@kalli.no>
2019-12-04 10:00:11 +0100

    Clean up

--- cmd/root.go
+++ cmd/root.go
@@ -16,15 +16,7 @@ var cfgFile string
 var rootCmd = &cobra.Command{
 	Use:   "ops",
 	Short: "ops-cli is a wrapper for devops tools",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Long:  `A wrapper for multiple devops tools...`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -38,16 +30,7 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ops.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
