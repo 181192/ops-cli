b7156b677b00d3502a687ed7a005aca9bad6bf89
181192 <k@kalli.no>
2019-12-06 00:12:43 +0100

    Download helmfile binary from github and store in config folder
    Use either latest or given version from config file .ops/ops.yaml

--- cmd/helmfile.go
+++ cmd/helmfile.go
@@ -1,10 +1,24 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	cmdUtil "github.com/181192/ops-cli/pkg/util"
 
+	getter "github.com/hashicorp/go-getter"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var helmfileBinary = cfgFolder + "/bin/helmfile"
+
+var helmfile = GitHubRelease{
+	account:  "roboll",
+	name:     "helmfile",
+	filename: "helmfile_linux_amd64",
+	version:  getVersion(),
+}
 
 // helmfileCmd represents the helmfile command
 var helmfileCmd = &cobra.Command{
@@ -16,10 +30,50 @@ var helmfileCmd = &cobra.Command{
 	- Apply CI/CD to configuration changes.
 	- Periodically sync to avoid skew in environments.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		cmdUtil.ExecuteCmd(cmd, "helmfile", args)
+		downloadIfNotExists(helmfile)
+		cmdUtil.ExecuteCmd(cmd, helmfileBinary, args)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(helmfileCmd)
+}
+
+// GitHubRelease represents a github release
+type GitHubRelease struct {
+	account  string
+	name     string
+	filename string
+	version  string
+}
+
+// getDownloadURL based on if version is latest or tagged
+func getDownloadURL(release GitHubRelease) string {
+	if release.version == "latest" {
+		return "https://github.com/" + release.account + "/" + release.name + "/releases/latest/download/" + release.filename
+	}
+	return "https://github.com/" + release.account + "/" + release.name + "/releases/download/" + release.version + "/" + release.filename
+}
+
+func getVersion() string {
+	version := viper.GetString("apps.helmfile")
+	if version == "" {
+		return "latest"
+	}
+	return version
+}
+
+func downloadIfNotExists(release GitHubRelease) {
+	if _, err := os.Stat(helmfileBinary); os.IsNotExist(err) {
+		progress := getter.WithProgress(cmdUtil.DefaultProgressBar)
+		url := getDownloadURL(release)
+
+		fmt.Println("Attempting to download helmfile, version: " + release.version)
+		err := getter.GetFile(helmfileBinary, url, progress)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		os.Chmod(helmfileBinary, 0775)
+	}
 }
