4b84785286266ababf54151db364112b13fca27f
181192 <k@kalli.no>
2019-12-06 11:06:34 +0100

    Add available release packages and link to GOOS/GOARCH.
    Do a PreRunE to check if helmfile is available

--- cmd/helmfile.go
+++ cmd/helmfile.go
@@ -1,23 +1,25 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
-
+	gh "github.com/181192/ops-cli/pkg/github"
 	cmdUtil "github.com/181192/ops-cli/pkg/util"
 
-	getter "github.com/hashicorp/go-getter"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
-var helmfileBinary = cfgFolder + "/bin/helmfile"
+var helmfile = gh.Release{
+	Account:       "roboll",
+	Name:          "helmfile",
+	LocalFileName: cfgFolder + "/bin/helmfile",
+}
 
-var helmfile = GitHubRelease{
-	account:  "roboll",
-	name:     "helmfile",
-	filename: "helmfile_linux_amd64",
-	version:  getVersion(),
+var artifactsNames = map[string]string{
+	"darwin/386":    "helmfile_darwin_386",
+	"darwin/amd64":  "helmfile_darwin_amd64",
+	"linux/386":     "helmfile_linux_386",
+	"linux/amd64":   "helmfile_linux_amd64",
+	"windows/386":   "helmfile_windows_386.exe",
+	"windows/amd64": "helmfile_windows_amd64.exe",
 }
 
 // helmfileCmd represents the helmfile command
@@ -29,9 +31,11 @@ var helmfileCmd = &cobra.Command{
 	- Keep a directory of chart value files and maintain changes in version control.
 	- Apply CI/CD to configuration changes.
 	- Periodically sync to avoid skew in environments.`,
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		return cmdUtil.RequireFile(helmfile.LocalFileName)
+	},
 	Run: func(cmd *cobra.Command, args []string) {
-		downloadIfNotExists(helmfile)
-		cmdUtil.ExecuteCmd(cmd, helmfileBinary, args)
+		cmdUtil.ExecuteCmd(cmd, helmfile.LocalFileName, args)
 	},
 }
 
@@ -39,41 +43,8 @@ func init() {
 	rootCmd.AddCommand(helmfileCmd)
 }
 
-// GitHubRelease represents a github release
-type GitHubRelease struct {
-	account  string
-	name     string
-	filename string
-	version  string
-}
-
-// getDownloadURL based on if version is latest or tagged
-func getDownloadURL(release GitHubRelease) string {
-	if release.version == "latest" {
-		return "https://github.com/" + release.account + "/" + release.name + "/releases/latest/download/" + release.filename
-	}
-	return "https://github.com/" + release.account + "/" + release.name + "/releases/download/" + release.version + "/" + release.filename
-}
-
-func getVersion() string {
-	version := viper.GetString("apps.helmfile")
-	if version == "" {
-		return "latest"
-	}
-	return version
-}
-
-func downloadIfNotExists(release GitHubRelease) {
-	if _, err := os.Stat(helmfileBinary); os.IsNotExist(err) {
-		progress := getter.WithProgress(cmdUtil.DefaultProgressBar)
-		url := getDownloadURL(release)
-
-		fmt.Println("Attempting to download helmfile, version: " + release.version)
-		err := getter.GetFile(helmfileBinary, url, progress)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		os.Chmod(helmfileBinary, 0775)
-	}
+func getHelmfileRelease() gh.Release {
+	helmfile.ArtifactName = gh.GetArtifactName(artifactsNames)
+	helmfile.Version = gh.GetVersion(helmfile)
+	return helmfile
 }
