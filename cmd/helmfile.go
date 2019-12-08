a15cba3c2ed6fd41989d0d26b5ee94150794342d
181192 <k@kalli.no>
2019-12-08 19:31:10 +0100

    Add struct to hold metadata about release

--- cmd/helmfile.go
+++ cmd/helmfile.go
@@ -1,25 +1,33 @@
 package cmd
 
 import (
-	gh "github.com/181192/ops-cli/pkg/github"
+	"github.com/181192/ops-cli/pkg/download"
 	cmdUtil "github.com/181192/ops-cli/pkg/util"
 
 	"github.com/spf13/cobra"
 )
 
-var helmfile = gh.Release{
-	Account:       "roboll",
-	Name:          "helmfile",
-	LocalFileName: cfgFolder + "/bin/helmfile",
-}
+var helmfileBinary = cfgFolder + "/bin/helmfile"
 
-var artifactsNames = map[string]string{
-	"darwin/386":    "helmfile_darwin_386",
-	"darwin/amd64":  "helmfile_darwin_amd64",
-	"linux/386":     "helmfile_linux_386",
-	"linux/amd64":   "helmfile_linux_amd64",
-	"windows/386":   "helmfile_windows_386.exe",
-	"windows/amd64": "helmfile_windows_amd64.exe",
+func newHelmfileRelease() *download.Release {
+
+	var artifactsNames = map[string]string{
+		"darwin/amd64":  "helmfile_darwin_amd64",
+		"linux/amd64":   "helmfile_linux_amd64",
+		"windows/amd64": "helmfile_windows_amd64.exe",
+	}
+
+	release := &download.Release{
+		Account:       "roboll",
+		Name:          "helmfile",
+		LocalFileName: helmfileBinary,
+	}
+
+	release.SetVersion()
+	release.SetArtifactName(artifactsNames)
+	release.SetDownloadURL()
+
+	return release
 }
 
 // helmfileCmd represents the helmfile command
@@ -32,19 +40,14 @@ var helmfileCmd = &cobra.Command{
 	- Apply CI/CD to configuration changes.
 	- Periodically sync to avoid skew in environments.`,
 	PreRunE: func(cmd *cobra.Command, args []string) error {
-		return cmdUtil.RequireFile(helmfile.LocalFileName)
+		return cmdUtil.RequireFile(helmfileBinary)
 	},
 	Run: func(cmd *cobra.Command, args []string) {
-		cmdUtil.ExecuteCmd(cmd, helmfile.LocalFileName, args)
+		// TODO add --helm-binary flag as default
+		cmdUtil.ExecuteCmd(cmd, helmfileBinary, args)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(helmfileCmd)
-}
-
-func getHelmfileRelease() gh.Release {
-	helmfile.ArtifactName = gh.GetArtifactName(artifactsNames)
-	helmfile.Version = gh.GetVersion(helmfile)
-	return helmfile
 }
