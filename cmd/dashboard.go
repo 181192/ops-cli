5ca82ec1bbcac2ad8b2fe8bdce7ba57220e0d311
181192 <k@kalli.no>
2019-12-12 10:28:55 +0100

    Add alertmanager dashboard command

--- cmd/dashboard.go
+++ cmd/dashboard.go
@@ -60,6 +60,7 @@ func init() {
 	dashboardCmd.AddCommand(promDashCmd())
 	dashboardCmd.AddCommand(grafanaDashCmd())
 	dashboardCmd.AddCommand(jaegerDashCmd())
+	dashboardCmd.AddCommand(alertmanagerDashCmd())
 
 	dashboardCmd.PersistentFlags().StringVarP(&kubeconfig, "kubeconfig", "c", "", "Kubernetes configuration file")
 	dashboardCmd.PersistentFlags().StringVar(&configContext, "context", "", "The name of the kubeconfig context to use")
@@ -144,6 +145,39 @@ func promDashCmd() *cobra.Command {
 			// only use the first pod in the list
 			return portForward(pl.Items[0].Name, namespace, "Prometheus",
 				"http://localhost:%d", 9090, client, cmd.OutOrStdout())
+		},
+	}
+
+	return cmd
+}
+
+func alertmanagerDashCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "alertmanager",
+		Short: "Open Alertmanager web UI",
+		Long:  `Open Alertmanager dashboard`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			client, err := kubernetes.NewClient(kubeconfig, configContext)
+			if err != nil {
+				return fmt.Errorf("failed to create k8s client: %v", err)
+			}
+
+			if namespace == "" {
+				namespace = "monitoring"
+			}
+
+			pl, err := client.PodsForSelector(namespace, "app=alertmanager")
+			if err != nil {
+				return fmt.Errorf("not able to locate Alertmanager pod: %v", err)
+			}
+
+			if len(pl.Items) < 1 {
+				return errors.New("no Alertmanager pods found")
+			}
+
+			// only use the first pod in the list
+			return portForward(pl.Items[0].Name, namespace, "Alertmanager",
+				"http://localhost:%d", 9093, client, cmd.OutOrStdout())
 		},
 	}
 
