01819bd00822b6abc358db02328901d8dfc31afe
181192 <k@kalli.no>
2020-01-29 16:39:18 +0100

    Load cluster config from file
    Clone flux git repository
    Add Flux reposity to helm
    Pull flux and helm-operator charts to flux git repository

--- go.mod
+++ go.mod
@@ -1,45 +1,32 @@
 module github.com/181192/ops-cli
 
-go 1.12
+go 1.13
 
 require (
-	github.com/aws/aws-sdk-go v1.24.1 // indirect
 	github.com/cheggaaa/pb v1.0.27
-	github.com/gogo/protobuf v1.3.1 // indirect
-	github.com/google/go-cmp v0.3.1 // indirect
-	github.com/googleapis/gnostic v0.3.1 // indirect
-	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
-	github.com/hashicorp/go-getter v1.4.0
-	github.com/hashicorp/go-version v1.2.0 // indirect
-	github.com/hashicorp/golang-lru v0.5.3 // indirect
-	github.com/imdario/mergo v0.3.7 // indirect
-	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
-	github.com/kris-nova/logger v0.0.0-20181127235838-fd0d87064b06
-	github.com/kris-nova/lolgopher v0.0.0-20180921204813-313b3abb0d9b // indirect
-	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/gofrs/flock v0.7.1
+	github.com/hashicorp/go-getter v1.4.1
+	github.com/kr/pretty v0.2.0
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/onsi/ginkgo v1.10.1
-	github.com/onsi/gomega v1.7.0
-	github.com/pelletier/go-toml v1.6.0 // indirect
-	github.com/pkg/errors v0.8.1
+	github.com/onsi/ginkgo v1.11.0
+	github.com/onsi/gomega v1.8.1
+	github.com/pkg/errors v0.9.1
 	github.com/rancher/wrangler v0.4.0
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/afero v1.2.2
-	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/cobra v0.0.5
-	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5
-	github.com/spf13/viper v1.6.1
+	github.com/spf13/viper v1.6.2
 	github.com/stretchr/testify v1.4.0
 	github.com/whilp/git-urls v0.0.0-20191001220047-6db9661140c0
-	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
-	golang.org/x/sys v0.0.0-20191219235734-af0d71d358ab // indirect
-	golang.org/x/tools v0.0.0-20191017205301-920acffc3e65 // indirect
-	google.golang.org/grpc v1.24.0 // indirect
-	gopkg.in/yaml.v2 v2.2.7 // indirect
-	k8s.io/api v0.17.0
-	k8s.io/apimachinery v0.17.0
-	k8s.io/client-go v0.17.0
+	helm.sh/helm/v3 v3.0.2
+	k8s.io/api v0.17.1
+	k8s.io/apimachinery v0.17.1
+	k8s.io/client-go v0.17.1
 	k8s.io/kops v1.11.0
 	sigs.k8s.io/yaml v1.1.0
 )
+
+replace github.com/docker/docker => github.com/moby/moby v0.7.3-0.20190826074503-38ab9da00309
+
+replace k8s.io/kubectl => k8s.io/kubectl v0.17.1
--- pkg/cmd/enable/repo.go
+++ pkg/cmd/enable/repo.go
@@ -2,30 +2,36 @@ package enable
 
 import (
 	"fmt"
-	"time"
-
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/client-go/kubernetes/scheme"
-
-	"sigs.k8s.io/yaml"
-
-	"github.com/pkg/errors"
-
 	"io/ioutil"
 	"os"
 
-	api "github.com/181192/ops-cli/pkg/apis/opscli.io/v1alpha1"
 	"github.com/181192/ops-cli/pkg/git"
+	"github.com/181192/ops-cli/pkg/helm"
+	"github.com/kr/pretty"
 
+	api "github.com/181192/ops-cli/pkg/apis/opscli.io/v1alpha1"
+	scheme "github.com/181192/ops-cli/pkg/generated/clientset/versioned/scheme"
+
+	"github.com/pkg/errors"
+	"github.com/rancher/wrangler/pkg/schemes"
+	logger "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/yaml"
 )
 
 func enableRepoCmd() *cobra.Command {
+	var gitOpts GitOptions
+	var fluxOpts FluxOptions
+
 	cmd := &cobra.Command{
 		Use:   "repo",
 		Short: "Set up a repo for gitops, installing Flux in the cluster and initializing its manifests.",
 		Long:  "",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			var err error
+
 			// client, err := kubernetes.NewClient(kubeconfig, configContext)
 			// if err != nil {
 			// 	return fmt.Errorf("failed to create k8s client: %v", err)
@@ -33,31 +39,74 @@ func enableRepoCmd() *cobra.Command {
 
 			// namespace := "monitoring"
 			// Create empty cluster config
+
+			scheme.AddToScheme(schemes.All)
+
 			api.NewAKSClusterConfig("", "", api.AKSClusterConfig{})
 
-			// Parse args / config file
+			clusterConfig, err := LoadConfigFromFile(clusterConfigFile)
+			if err != nil {
+				return err
+			}
 
-			// print config
+			logger.Debugf("%# v", pretty.Formatter(clusterConfig))
+
+			gitClient := git.NewGitClient(git.ClientParams{
+				PrivateSSHKeyPath: gitOpts.PrivateSSHKeyPath,
+			})
+
+			logger.Infof("Cloning %s", gitOpts.URL)
+			options := git.CloneOptions{
+				URL:       gitOpts.URL,
+				Branch:    gitOpts.Branch,
+				Bootstrap: true,
+			}
+
+			cloneDir, err := gitClient.CloneRepoInTmpDir("ops-cli-bootstrap-", options)
+			if err != nil {
+				return errors.Wrapf(err, "cannot clone repository %s", gitOpts.URL)
+			}
+
+			cleanCloneDir := false
+			defer func() {
+				if cleanCloneDir {
+					_ = gitClient.DeleteLocalRepo()
+					logger.Debugf("Deleting temporary local clone of %s at %s", gitOpts.URL, cloneDir)
+				} else {
+					logger.Warningf("You may find the local clone of %s at %s", gitOpts.URL, cloneDir)
+				}
+			}()
+
+			repoName := "fluxcd"
+			repoURL := "https://charts.fluxcd.io"
+			var chartName string
+			var chartVersion string
+			if err := helm.AddRepository(repoName, repoURL); err != nil {
+				logger.Fatalf("Failed to add registry %s %s. %s", repoName, repoURL, err)
+			}
+
+			chartName = repoName + "/flux"
+			chartVersion = "1.1.0"
+			if err := helm.PullChartUntarToDir(chartName, chartVersion, cloneDir+"/flux-manifests"); err != nil {
+				logger.Fatalf("Failed to pull chart %s. %s", chartName, err)
+			}
+
+			chartName = repoName + "/helm-operator"
+			chartVersion = "0.6.0"
+			if err := helm.PullChartUntarToDir(chartName, chartVersion, cloneDir+"/flux-manifests"); err != nil {
+				logger.Fatalf("Failed to pull chart %s. %s", chartName, err)
+			}
+
+			// cleanCloneDir = true
 
 			return nil
 		},
 	}
 
+	cmd = configureGitOptions(cmd, &gitOpts)
+	cmd = configureFluxOptions(cmd, &fluxOpts)
+
 	return cmd
-}
-
-type InstallOpts struct {
-	GitOptions  git.Options
-	GitPaths    []string
-	GitLabel    string
-	GitFluxPath string
-	Namespace   string
-	Timeout     time.Duration
-	WithHelm    bool
-}
-
-func configureRepositoryCmd(cmd *cobra.Command) {
-
 }
 
 // LoadConfigFromFile loads ClusterConfig from configFile
