f5832118df14546f5e31a3db8968404f5d95e239
181192 <k@kalli.no>
2020-02-07 17:25:58 +0100

    Ignore in CI

--- pkg/git/gitfindroot/find_test.go
+++ pkg/git/gitfindroot/find_test.go
@@ -3,10 +3,13 @@ package gitfindroot
 import (
 	"testing"
 
+	"github.com/181192/ops-cli/pkg/testutils"
+
 	logger "github.com/sirupsen/logrus"
 )
 
 func TestRootIsFound(t *testing.T) {
+	testutils.SkipCI(t)
 	response, err := Repo()
 	if err != nil {
 		logger.Fatalf("Error: %s", err.Error())
