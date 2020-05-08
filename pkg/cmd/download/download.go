ac8206ee47524176c083a83c04668c33c90a4c4d
Kristoffer-Andre Kalliainen <Kristoffer-Andre.Kalliainen@stacc.com>
2020-05-08 20:40:00 +0200

    Check for latest kubectl version if release.Version is set to latest
    Remove cfgFolder, use util.GetconfigDirectory()

--- pkg/cmd/download/download.go
+++ pkg/cmd/download/download.go
@@ -1,24 +1,8 @@
 package download
 
 import (
-	"fmt"
-	"os"
-
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 )
-
-// TODO get cfgFolder from root
-var cfgFolder = getHome() + "/.ops"
-
-func getHome() string {
-	home, err := homedir.Dir()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	return home
-}
 
 // downloadCmd represents the download command
 var downloadCmd = &cobra.Command{
--- pkg/cmd/download/kubectl.go
+++ pkg/cmd/download/kubectl.go
@@ -2,11 +2,15 @@ package download
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"strings"
 
 	"github.com/181192/ops-cli/pkg/download"
-	cmdUtil "github.com/181192/ops-cli/pkg/util"
+	"github.com/181192/ops-cli/pkg/util"
 	"github.com/hashicorp/go-getter"
+	logger "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -14,7 +18,7 @@ import (
 // https://storage.googleapis.com/kubernetes-release/release/v1.16.0/bin/darwin/amd64/kubectl
 // https://storage.googleapis.com/kubernetes-release/release/v1.16.0/bin/windows/amd64/kubectl.exe
 
-var kubectlBinary = cfgFolder + "/bin/kubectl"
+var kubectlBinary = util.GetConfigDirectory() + "/bin/kubectl"
 
 type kubectlRelease struct {
 	*download.Release
@@ -49,16 +53,30 @@ var kubectlCmd = &cobra.Command{
 	Short: "kubectl controls the Kubernetes cluster manager",
 	Long:  `kubectl controls the Kubernetes cluster manager.`,
 	PreRunE: func(cmd *cobra.Command, args []string) error {
-		return cmdUtil.RequireFile(kubectlBinary)
+		return util.RequireFile(kubectlBinary)
 	},
 	Run: func(cmd *cobra.Command, args []string) {
-		cmdUtil.ExecuteCmd(cmd, kubectlBinary, args)
+		util.ExecuteCmd(cmd, kubectlBinary, args)
 	},
 }
 
 func (release *kubectlRelease) setDownloadURL() *kubectlRelease {
 	if release.Version == "latest" {
-		release.Version = "v1.16.0"
+		kubectlVersion := "v1.16.0"
+
+		resp, err := http.Get("https://storage.googleapis.com/kubernetes-release/release/stable.txt")
+		if err != nil {
+			logger.Warnf("Failed to get latest stable version of kubectl %s", err)
+			release.Version = kubectlVersion
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+
+		if resp.StatusCode != http.StatusOK {
+			logger.Warnf("Failed to read latest version, default to %s", kubectlVersion)
+			release.Version = kubectlVersion
+		}
+		release.Version = strings.TrimSpace(string(body))
 	}
 	release.URL = "https://storage.googleapis.com/kubernetes-release/release/" + release.Version + "/" + release.ArtifactName
 	return release
@@ -68,7 +86,7 @@ func (release *kubectlRelease) DownloadIfNotExists() error {
 	if _, err := os.Stat(release.LocalFileName); os.IsNotExist(err) {
 		progress := getter.WithProgress(download.DefaultProgressBar)
 
-		fmt.Printf("Attempting to download %s, version %s, to %q\n", release.Name, release.Version, release.LocalFileName)
+		logger.Infof("Attempting to download %s, version %s, to %q\n", release.Name, release.Version, release.LocalFileName)
 
 		err := getter.GetFile(release.LocalFileName, release.URL, progress)
 		if err != nil {
@@ -80,7 +98,7 @@ func (release *kubectlRelease) DownloadIfNotExists() error {
 			return fmt.Errorf("%s\nFailed chmod", err)
 		}
 	} else {
-		fmt.Printf("%s already exists at %s\n", release.Name, release.LocalFileName)
+		logger.Infof("%s already exists at %s\n", release.Name, release.LocalFileName)
 	}
 	return nil
 }
