e610d953e92754c2f5a4cfe291a330b433b28009
Kristoffer-Andre Kalliainen <Kristoffer-Andre.Kalliainen@stacc.com>
2020-05-08 20:09:58 +0200

    Switch to logrus logging in pkg/download

--- pkg/download/download.go
+++ pkg/download/download.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/hashicorp/go-getter"
 	"github.com/spf13/viper"
+
+	logger "github.com/sirupsen/logrus"
 )
 
 // Release represents a github release
@@ -36,14 +38,14 @@ func (release *Release) DownloadIfNotExists() error {
 	if _, err := os.Stat(release.LocalFileName); os.IsNotExist(err) {
 		progress := getter.WithProgress(DefaultProgressBar)
 
-		fmt.Printf("Attempting to download %s, version %s, to %q\n", release.Name, release.Version, release.LocalFileName)
+		logger.Infof("Attempting to download %s, version %s, to %q\n", release.Name, release.Version, release.LocalFileName)
 		err := getter.GetFile(release.LocalFileName, release.URL, progress)
 		if err != nil {
 			return fmt.Errorf("%s\nFailed to to download external binaries", err)
 		}
 		os.Chmod(release.LocalFileName, 0775)
 	} else {
-		fmt.Printf("%s already exists at %s\n", release.Name, release.LocalFileName)
+		logger.Infof("%s already exists at %s\n", release.Name, release.LocalFileName)
 	}
 	return nil
 }
@@ -52,7 +54,7 @@ func (release *Release) DownloadIfNotExists() error {
 func (release *Release) SetVersion() *Release {
 	version := viper.GetString("apps. " + release.Name)
 	if version == "" {
-		fmt.Println("No version set in config, using latest")
+		logger.Info("No version set in config, using latest")
 		version = "latest"
 	}
 
@@ -65,7 +67,7 @@ func (release *Release) SetArtifactName(artifacts map[string]string) *Release {
 	platform := runtime.GOOS + "/" + runtime.GOARCH
 	artifact, found := artifacts[platform]
 	if !found {
-		fmt.Printf("Unsupported os/platform %s\n", platform)
+		logger.Info("Unsupported os/platform %s\n", platform)
 	}
 
 	release.ArtifactName = artifact
