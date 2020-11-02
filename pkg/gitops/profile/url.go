81f1a2dc91fd82c4ed5d179fd0d02c7e427f45e4
181192 <k@kalli.no>
2020-11-02 13:43:18 +0100

    Add feature to read profiles from configuration file

--- pkg/gitops/profile/url.go
+++ pkg/gitops/profile/url.go
@@ -4,20 +4,26 @@ import (
 	"fmt"
 
 	"github.com/181192/ops-cli/pkg/git"
+	logger "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
 // RepositoryURL returns the full Git repository URL corresponding to the
 // provided "quickstart profile" mnemonic a.k.a. short name. If a valid Git URL
 // is provided, this function returns it as-is.
-func RepositoryURL(quickstartArgument string) (string, error) {
-	if git.IsGitURL(quickstartArgument) {
-		return quickstartArgument, nil
+func RepositoryURL(profileArgument string) (string, error) {
+	if git.IsGitURL(profileArgument) {
+		return profileArgument, nil
 	}
-	if quickstartArgument == "app-dev" {
+	if profile := viper.GetString("profiles." + profileArgument); profile != "" {
+		logger.Info(fmt.Sprintf("Using %s profile %s from config %s", profileArgument, profile, viper.ConfigFileUsed()))
+		return profile, nil
+	}
+	if profileArgument == "app-dev" {
 		return "https://github.com/weaveworks/eks-quickstart-app-dev", nil
 	}
-	if quickstartArgument == "appmesh" {
+	if profileArgument == "appmesh" {
 		return "https://github.com/weaveworks/eks-appmesh-profile", nil
 	}
-	return "", fmt.Errorf("invalid URL or unknown Quick Start profile: %s", quickstartArgument)
+	return "", fmt.Errorf("invalid URL or unknown profile: %s", profileArgument)
 }
